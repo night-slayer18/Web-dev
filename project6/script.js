aba9dba2182c2c9d9915c8fa36f59bf47780a6a7
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-01-31 23:54:09 +0530

    table navigation

--- project6/script.js
+++ project6/script.js
@@ -2,3 +2,19 @@ console.log(document.body.firstChild)
 console.log(document.body.lastChild)
 console.log(document.body.childNodes)
 console.log(Array.from(document.body.childNodes))
+
+console.log(document.body.firstChild)
+console.log(document.body.firstElementChild)
+console.log(document.body.firstElementChild.parentElement)
+console.log(document.body.firstElementChild.parentNode)
+
+let b = document.body
+console.log("first child" , b.firstChild)
+console.log("first element child" , b.firstElementChild)
+
+const BodyBG = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+
+let t = document.body.firstElementChild.firstElementChild
+console.log(t.rows)
